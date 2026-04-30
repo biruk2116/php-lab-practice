54d19ec067789bb1fb64cb969d5923b5db8dc60b
biruk2116 <brukbelay61@gmail.com>
2026-04-30 14:36:40 +0300

    add: create student update form with database update functionality

---
+++ Update.php
@@ -0,0 +1,20 @@
+<?php include 'db.php'; ?>
+<form method="POST">
+ ID: <input type="number" name="id"><br>
+ New Name: <input type="text" name="name"><br>
+ New Age: <input type="number" name="age"><br>
+ New Gender: <input type="text" name="gender"><br>
+ <button type="submit" name="update">Update</button>
+</form>
+<?php
+if (isset($_POST['update'])) {
+ $id = $_POST['id'];
+ $name = $_POST['name'];
+ $age = $_POST['age'];
+ $gender = $_POST['gender'];
+ $stmt = $conn->prepare("UPDATE student SET name=?, age=?, gender=? WHERE id=?");
+ $stmt->bind_param("sisi", $name, $age, $gender, $id);
+ $stmt->execute();
+ echo "Student updated successfully!";
+}
+?>
