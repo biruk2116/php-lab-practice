7c77fd1171565c9d657441471f332d8749886f57
biruk2116 <brukbelay61@gmail.com>
2026-04-30 14:35:53 +0300

    refactor: simplify database connection variables

--- db.php
+++ db.php
@@ -1,11 +1,8 @@
 <?php
-
-$db_server = "localhost";
-$db_user   = "root";
-$db_pass   = "";
-$db_name   = "DBU";
-$db_port   = 3306;
-
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "DBU";
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
