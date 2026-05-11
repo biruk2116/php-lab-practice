6b007fba8eb9320aa0587b593db32f2c9e8380b6
biruk2116 <brukbelay61@gmail.com>
2026-05-11 11:18:02 +0300

    fix: correct formatting and binding parameters in newinsert.php

--- newinsert.php
+++ newinsert.php
@@ -1,15 +1,15 @@
-<?php include 'newdb.php';?>
+<?php include 'newdb.php'; ?>
 
-<form  method="POST">
-   ID: <input type="number" name="id"><br>
-   NAME: <input type="text" name= "name"><br>
-   AGE : <input type="number" name="age"><br>
-   GENDER: <input type="text" name="gender"><br>
-   <input type="submit" name="submit" value="insert">
+<form method="POST">
+    ID: <input type="number" name="id"><br>
+    NAME: <input type="text" name="name"><br>
+    AGE : <input type="number" name="age"><br>
+    GENDER: <input type="text" name="gender"><br>
+    <input type="submit" name="submit" value="insert">
 </form>
-<?php 
+<?php
 
-if (isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $id = $_POST['id'];
     $name = $_POST['name'];
     $age = $_POST['age'];
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])){
 // prepared statement
 $stmt = $conn->prepare("INSERT INTO stud VALUES (?,?,?,?)");
 
-$stmt->bind_param("siss",$id,$name,$age,$gender);
+$stmt->bind_param("isis", $id, $name, $age, $gender);
 $stmt->execute();
 echo "student inserted successfully!";
 
