79ed7d4c4aa44587547a09b44008e4bd1a4b71e8
biruk2116 <brukbelay61@gmail.com>
2026-05-11 10:40:23 +0300

    add: create new insert student form in newinsert.php

---
+++ newinsert.php
@@ -0,0 +1,14 @@
+<?php include 'newdb.php';?>
+
+<form  method="POST">
+   ID: <input type="number" name="id"><br>
+   NAME: <input type="text" name= "name"><br>
+   AGE : <input type="number" name="age"><br>
+   GENDER: <input type="text" name="gender"><br>
+   <input type="submit" name="submit" value="insert">
+</form>
+<?php 
+
+
+?>
+
