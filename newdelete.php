cbac5a6fc92b94b18b3a8403651258e3357fabad
biruk2116 <brukbelay61@gmail.com>
2026-05-11 11:30:52 +0300

    fix: correct SQL syntax error in delete student functionality in newdelete.php

--- newdelete.php
+++ newdelete.php
@@ -4,3 +4,13 @@
     <button type="submit" name="delete">delete</button>
 </form>
 
+<?php 
+     if(isset($_POST["delete"])){
+        $id=$_POST["id"];
+        $stmt=$conn->prepare("DELTE FROM stud WHERE ID=?");
+        $stmt->bind_param("i",$id);
+        $stmt->execute();
+        echo "student deleted successfully!";
+
+     }
+?>
